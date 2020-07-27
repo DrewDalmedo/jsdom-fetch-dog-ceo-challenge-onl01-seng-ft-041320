a0710a00881828e372ad77ddcdcabc956b628736
DrewDalmedo <andrewdalmedo@gmail.com>
2020-07-27 08:59:37 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function addImage(image) {
+    let img = document.createElement("img");
+    img.src = image
+    document.body.appendChild(img)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(imgUrl)
+    .then( (response) => {
+        return response.json();
+    })
+    .then( (json) => {
+        console.log(json)
+        let images = json.message
+        images.forEach(addImage)
+    })
+    
+});
